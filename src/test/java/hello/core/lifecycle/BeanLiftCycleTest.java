aab64959511748d55d61458d2fa28702ab0c029f
myeongseok.seo <myeongseok.seo@softcamp.co.kr>
2022-12-29 16:44:51 +0900

    빈 생명주기 콜백

---
+++ src/test/java/hello/core/lifecycle/BeanLiftCycleTest.java
@@ -0,0 +1,16 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BeanLiftCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+}
